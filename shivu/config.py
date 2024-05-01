4553c82566cfd97f6c47edfca3acd597906bf02e
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-05-01 11:05:47 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "6584789596"
     sudo_users = "6101457748", "5932230962", "6100011620", "2010819209", "5702598840", "1813320767", "6154972031", "6584789596", "5702598840"
     GROUP_ID = -1002133191051
-    TOKEN = "6462620320:AAGIBSjD_PmyiSNPmF3kdYsO0G2TSMpQe9M"
+    TOKEN = "7078901224:AAHFsIYp9W-O26BDw_LT_pQFqQVwcJUlIlY"
     mongo_url = "mongodb+srv://Srikanta:srikanta@cluster0.xzbil3m.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg"]
     SUPPORT_CHAT = "Grabbing_Your_WH_Group"
